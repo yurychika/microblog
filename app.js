faab96e7fbb12ce0db9c4f5ba2b3266d86746d34
qi chen <yurychika@gmail.com>
2013-06-16 17:12:05 +0800

    add mongo db support

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path'),
+  util = require('util');
 
 var app = express();
 
@@ -15,21 +16,48 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.set('view options', {
+	layout: true
+});
+
+
+// app.use(function(req, res){
+// 	console.log('this is the start of the request');
+// });
+
+app.use(function(req, res, next){
+	console.log('this is the start of the request');
+	next();
+
+});
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+// app.use(express.router(routes));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res){
+	console.log('this is the start of the request');
+});
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/stylesheets/style.css', routes.index);
+routes(app);
+// app.get('/', function(req, res, next){
+// 	console.log('get all routers');
+// 	// res.send(util.inspect(req));
+// 	// res.send('params is:' + req.params.name);
+
+// 	res.send('params is:' + req.param('name') + ' ' + req.query.name);
+// 	// next();
+// });
+// app.get('/', routes.index);
+// app.get('/users', user.list);
+// app.get('/stylesheets/style.css', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+module.exports = function(app){
+	app.get('/', function(req, res){
+	  res.render('index', { title: 'Express', list: [1,2,3] });
+	});
+
+	app.get('/reg', function(req, res){
+	  res.render('reg', { title: 'Express', list: [1,2,3] });
+	});	
+
+	app.post('/reg', function(req, res){
+	  res.render('doReg', { title: 'Express', list: [1,2,3] });
+	});	
+}
